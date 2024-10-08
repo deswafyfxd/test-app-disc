ac9da8d8ad49ada3a0254fc06f6680da048f2b95
Ghred dase <129958145+deswafyfxd@users.noreply.github.com>
2024-10-08 14:01:22 +0530

    Update retrieve_email.py

--- retrieve_email.py
+++ retrieve_email.py
@@ -5,7 +5,7 @@ import os
 
 # Function to get accounts from GitHub
 def fetch_accounts():
-    url = 'https://raw.githubusercontent.com/<username>/<repository>/main/accounts.yml'
+    url = 'https://raw.githubusercontent.com/deswafyfxd/disc-data-strore/main/accounts.yml'
     response = requests.get(url)
     return yaml.safe_load(response.text)['accounts']
 
